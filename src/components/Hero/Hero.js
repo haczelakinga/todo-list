893296037c274317b834664724b8547151774f0c
Kinga Haczela <haczelakinga@gmail.com>
2020-06-05 14:19:18 +0200

    Create Column component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-const Hero = () => (
+
+const Hero = props => (
     <header className={styles.component}>
-        <h2 className={styles.title}>Things to do</h2>
-        <img className={styles.image} src="src/assets/space.png"></img>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img className={styles.image} src={props.imgSrc}></img>
     </header>
 )
+
+Hero.propTypes = {
+    titleText: PropTypes.node.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  };
 
 export default Hero;
         
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from './../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from './../Column/Column';
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        imgSrc: PropTypes.string.isRequired,
+        children: PropTypes.node,
+      }
+      static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+      }
     render() {
         return (
         <section className = {styles.component}>
-            <Hero />
+            <Hero titleText = {this.props.title} imgSrc = {this.props.imgSrc}/>
+            <div className={styles.description}>
+                {this.props.children}
+            </div>
+            <div className = {styles.columns}>
+                <Column colTitle = {'Animal'}/>
+                <Column colTitle = {'Plants'}/>
+                <Column colTitle = {'Mineral'}/>
+            </div>
         </section>
         )
     }

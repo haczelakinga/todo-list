14af87291e1b496ed32dc677d160551244fb9053
Kinga Haczela <haczelakinga@gmail.com>
2020-06-12 14:08:25 +0200

    Add search component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import styles from './App.scss';
 import List from './../List/ListContainer';
-// import {pageContents, listData} from '../../data/dataStore';
-// import Creator from './../Creator/Creator';
 import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
 
 class App extends React.Component {
-
-  // state = {
-  //   lists: [],
-  // }
 
   static propTypes = {
     title: PropTypes.node,
@@ -17,43 +12,13 @@ class App extends React.Component {
     lists: PropTypes.array,
   }
 
-  // addList(title){
-  //   this.setState(state => (
-  //     {
-  //       lists: [
-  //         ...state.lists,
-  //         {
-  //           title:title,
-  //           description:listData.description,
-  //           image:listData.image,
-  //           columns:listData.columns,
-  //           key: state.lists.length ? state.lists[state.lists.length-1].key+1 : 0,
-  //         },
-  //       ],
-  //     }
-  //   ));
-  // }
-
   render() {
-    const {title, subtitle, lists} = this.props;
+    const {title, subtitle, lists, } = this.props;
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-        {/* <List
-          title={listData.title}
-          description={listData.description}
-          image={listData.image}
-          columns={listData.columns}
-        /> */}
-        {/* <div>
-          {this.state.lists.map(({key, ...listProps}) => (
-            <List key={key} {...listProps} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={'Add new list'} action={title => this.addList(title)}/>
-        </div> */}
+        <Search />
         {lists.map(listData => (
           <List key={listData.id} {...listData} />
         ))}
